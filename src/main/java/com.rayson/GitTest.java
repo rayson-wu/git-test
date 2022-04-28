e07a6cce8a4863412b24ea27b8625e864b96c21e
rayson <215475705@qq.com>
2022-04-28 10:20:34 +0800

    hot-fix commit2

--- src/main/java/com.rayson/GitTest.java
+++ src/main/java/com.rayson/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4 hot-fix changed");
+        System.out.println("hot-fix test conflict");
     }
 }
