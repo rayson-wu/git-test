0540c2f2a7f0d3ca26b41717f47c2f172056c34f
rayson <215475705@qq.com>
2022-04-28 10:07:33 +0800

    首次提交

---
+++ src/main/java/com.rayson/GitTest.java
@@ -0,0 +1,12 @@
+package com.rayson;
+
+/**
+ * Author: rayson
+ * Description:
+ * Date: 2022/4/28 10:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
